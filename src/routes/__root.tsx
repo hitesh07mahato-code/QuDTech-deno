0cb4a55f82ca2d876dcde77e30676d80c4a7e109
Hitesh <hitesh07mahato@gmail.com>
2026-09-10 00:20:42 +0530

    Update favicon assets and icon link tags in root layout

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -98,7 +98,9 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "icon", href: "/favicon.ico", sizes: "any" },
+      { rel: "icon", href: "/favicon.png", type: "image/png" },
+      { rel: "apple-touch-icon", href: "/apple-touch-icon.png" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
